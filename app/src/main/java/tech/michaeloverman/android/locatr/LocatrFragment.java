8fb974fd76daca98b2a30af313fc044dc7f1d011
michaeloverman <overman.michael@gmail.com>
2016-08-03 12:23:51 -0400

    receiving location from FusedLocationApi

--- app/src/main/java/tech/michaeloverman/android/locatr/LocatrFragment.java
+++ app/src/main/java/tech/michaeloverman/android/locatr/LocatrFragment.java
@@ -1,8 +1,10 @@
 package tech.michaeloverman.android.locatr;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,12 +14,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by Michael on 8/3/2016.
  */
 public class LocatrFragment extends Fragment {
+    private static final String TAG = "LocatrFragment";
     private ImageView mImageView;
     private GoogleApiClient mClient;
 
@@ -78,5 +83,32 @@ public class LocatrFragment extends Fragment {
 
         MenuItem searchItem = menu.findItem(R.id.action_locate);
         searchItem.setEnabled(mClient.isConnected());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_locate:
+                findImage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    private void findImage() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "Got a fix: " + location);
+                    }
+                });
     }
 }
